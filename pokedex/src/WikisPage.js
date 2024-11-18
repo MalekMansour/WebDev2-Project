a0fd5c67a917ba54bcb0b4fc9282e1469cec82e7
Malek Mansour <malek24mansour@gmail.com>
2024-11-17 22:23:44 -0700

    Update WikisPage.js

---
+++ pokedex/src/WikisPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./WikisPage.css";
+
+function WikisPage() {
+  const wikis = [
+    { name: "Bulbapedia", url: "https://bulbapedia.bulbagarden.net/" },
+    { name: "Serebii", url: "https://www.serebii.net/" },
+    { name: "Pokémon Database", url: "https://pokemondb.net/" },
+    { name: "Pokémon Wiki", url: "https://pokemon.fandom.com/" },
+    { name: "Smogon", url: "https://www.smogon.com/" },
+  ];
+
+  return (
+    <div className="wikis-page">
+      <h1>Pokémon Wikis</h1>
+      <ul>
+        {wikis.map((wiki) => (
+          <li key={wiki.name}>
+            <a href={wiki.url} target="_blank" rel="noopener noreferrer">
+              {wiki.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default WikisPage;
