b7176c3b1f12098628d2dab31f3b303b3b76d4d5
Malek Mansour <malek24mansour@gmail.com>
2024-11-21 11:23:06 -0700

    Update DarkModeContext.js

--- pokedex/src/DarkModeContext.js
+++ pokedex/src/DarkModeContext.js
@@ -1,21 +1,21 @@
 import React, { createContext, useState, useEffect } from "react";
 
-// Create the context
 export const DarkModeContext = createContext();
 
-// Create a provider component
 export const DarkModeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
     return localStorage.getItem("darkMode") === "true";
   });
 
   useEffect(() => {
-    localStorage.setItem("darkMode", isDarkMode);
     document.body.classList.toggle("dark-mode", isDarkMode);
+    localStorage.setItem("darkMode", isDarkMode);
   }, [isDarkMode]);
 
+  const toggleDarkMode = () => setIsDarkMode((prev) => !prev);
+
   return (
-    <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
