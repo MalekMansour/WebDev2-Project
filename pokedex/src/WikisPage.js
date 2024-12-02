3f97fe3fb364452942794837b2214b28b8598f1b
Malek Mansour <malek24mansour@gmail.com>
2024-12-01 23:23:14 -0700

    Update WikisPage.js

--- pokedex/src/WikisPage.js
+++ pokedex/src/WikisPage.js
@@ -3,6 +3,7 @@ import "./WikisPage.css";
 import sunIcon from "./assets/sun.png";
 import moonIcon from "./assets/moon.png";
 
+// Importing some basic Pokemon Wikis
 function WikisPage() {
   const [darkMode, setDarkMode] = useState(false);
   const wikis = [
