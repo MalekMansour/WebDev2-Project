f0c976783db2ef2bf53a1412e4eeb8737edd2087
Malek Mansour <malek24mansour@gmail.com>
2024-12-03 23:33:57 -0700

    Update AuthContext.js

---
+++ pokedex/src/AuthContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { app } from "./firebaseConfig";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
