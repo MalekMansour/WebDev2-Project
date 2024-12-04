4693824c980075e3cf0d013cd8eafa9aa4222d39
Malek Mansour <malek24mansour@gmail.com>
2024-12-04 01:07:40 -0700

    Update LoginPage.js

--- pokedex/src/LoginPage.js
+++ pokedex/src/LoginPage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { useAuth } from "./AuthContext";
+import "./LoginPage.css";
+import pikachu from "./assets/pikachu.png";
+import bulbasaur from "./assets/bulbasaur.png";
 
 function LoginPage() {
   const { signInWithGoogle } = useAuth();
@@ -10,6 +13,8 @@ function LoginPage() {
       <button onClick={signInWithGoogle} className="login-button">
         Sign in with Google
       </button>
+      <img src={pikachu} alt="Pikachu" className="pokemon-image-left" />
+      <img src={bulbasaur} alt="Bulbasaur" className="pokemon-image-right" />
     </div>
   );
 }
